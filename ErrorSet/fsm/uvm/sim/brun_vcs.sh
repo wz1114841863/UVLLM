c093b5e0404a6596728b3acb4501e46171b39bb3
amyuch <105541599+amyuch@users.noreply.github.com>
2024-12-05 10:43:25 +0800

    Add files via upload

---
+++ ErrorSet/fsm/uvm/sim/brun_vcs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+
+test_name="test"    # uvm_test class
+top_mod="tb_top"          # top module name
+
+export UVM_HOME=/YOUR/PATH/TO/UVM
+export VCS_HOME=/YOUR/PATH/TO/VCS
+
+
+vlogan -full64 -sverilog -f ../tb/src_list.f -ntb_opts uvm +incdir+$UVM_HOME/src 
+if [ $? -ne 0 ]; then
+    exit -1
+fi
+
+
+gcc -c -o c_model.o ../tb/ref_model.c -I$UVM_HOME/src/dpi
+if [ $? -ne 0 ]; then
+    exit -1
+fi
+
+# Elaborate the design
+
+vcs -full64 -debug_acc+all+dmptf -debug_region+cell+encrypt -timescale=1ns/1ps +vcs+dumpvars+dumpfile=test.vcd $top_mod -o simv $UVM_HOME/src/dpi/uvm_dpi.cc c_model.o -CFLAGS -DVCS
+#vcs -full64 -debug_acc+all+dmptf -debug_region+cell+encrypt -timescale=1ns/1ps +vcs+dumpvars+dumpfile=test.vcd $top_mod -o simv $UVM_HOME/src/dpi/uvm_dpi.cc -CFLAGS -DVCS
+
+if [ $? -ne 0 ]; then
+    exit -1
+fi
+
+# Run the simulation
+./simv +UVM_TESTNAME=$test_name +UVM_VERBOSITY=UVM_HIGH +ntb_random_seed=$seed -l simv.log
+
